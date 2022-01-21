df1398d2db3816de88b9e6685900924ff64656c8
David Shin <shin.david93@gmail.com>
2022-01-21 12:25:46 -0800

    Set up core flask elements

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,8 @@
 from app import app
+from flask import render_template
+from app.models import User
+from forms import RegisterForm
+
+@app.route('/')
+def index():
+    return render_template('index.html')
