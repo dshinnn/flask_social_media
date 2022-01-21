11605e77fc3c5362af3f39f1556eba5fc6a331ff
David Shin <shin.david93@gmail.com>
2022-01-21 14:07:57 -0800

    Add flash messaging

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from app.models import User
 from app.forms import RegisterForm
 
@@ -16,8 +16,7 @@ def register():
         email = form.email.data
         password = form.password.data
         User(username=username, email=email, password=password)
-        print('Username has been successfully created!')
+        flash('Username has been successfully created!', 'success')
         return redirect(url_for('index'))
-    else:
-        print('FAILED')
+        
     return render_template('register.html', form=form)
